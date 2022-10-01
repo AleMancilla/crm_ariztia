c2cfffad8c1908d4796c2eff7ff96b2f101aa888
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2022-10-01 15:03:36 -0400

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:ariztia_crm/firebase_options.dart';
 import 'package:ariztia_crm/login/presentation/page/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
